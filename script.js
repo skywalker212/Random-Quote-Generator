c3f11ad5dd4713ffebef2753470ef01fedf1eacb
Akash Gajjar <akashgajjar8@gmail.com>
2017-03-29 18:48:52 +0530

    used Quote API to get random quotes

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+    $.ajax({
+        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+        , cache: false
+        , dataType: "json"
+        , success: function (json) {
+            $(".quote").html(json[0].content + json[0].title);
+        }
+    });
     $("#butn").on("click", function () {
-        $(".quote").html("you have clicked the button!");
+        $.ajax({
+            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+            , cache: false
+            , dataType: "json"
+            , success: function (json) {
+                $(".quote").html(json[0].content + json[0].title);
+            }
+        });
     });
 });
