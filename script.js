44617f650da249997bf8e03f64cf4ba3e8285a7c
Akash Gajjar <akashgajjar8@gmail.com>
2017-03-31 21:37:11 +0530

    used a different API

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 $(document).ready(function () {
     $.ajax({
-        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+        url: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
         , cache: false
         , dataType: "json"
         , success: function (json) {
-            $(".quote").html("<p>" + json[0].content + "</p>" + "<hr width=20%>" + "<footer>" + json[0].title + "</footer>");
+            $(".quote").html("<p>" + json.quoteText + "</p>" + "<hr width=20%>" + "<footer>" + json.quoteAuthor + "</footer>");
         }
     });
     $("#butn").on("click", function () {
         $.ajax({
-            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+            url: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
             , cache: false
             , dataType: "json"
             , success: function (json) {
-                $(".quote").html('<p class="main-quo">' + json[0].content + "</p>" + "<hr width=20%>" + '<footer class="auth">' + json[0].title + "</footer>");
+                $(".quote").html("<p>" + json.quoteText + "</p>" + "<hr>" + "<footer>" + json.quoteAuthor + "</footer>");
             }
         });
+        $("#tweet").attr("href","https://twitter.com/intent/tweet?");
+    });
+    $("#tweet").on("click",function(){
+      console.log("button was clicked!");
     });
 });
