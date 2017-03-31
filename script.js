6386499768b3228b28c750c619c3aa726ea0d622
Deadpool1994 <mhtrami@gmail.com>
2017-04-01 01:26:56 +0530

    add href on success of AJAX

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 $(document).ready(function () {
-    $.ajax({
-        url: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
-        , cache: false
-        , dataType: "json"
-        , success: function (json) {
-            $(".quote").html("<p>" + json.quoteText + "</p>" + "<hr width=20%>" + "<footer>" + json.quoteAuthor + "</footer>");
-        }
-    });
+
+var ajaxFunction = function(){
+  $.ajax({
+      url: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
+      , cache: false
+      , dataType: "json"
+      , success: function (json) {
+          $(".quote").html("<p>" + json.quoteText + "</p>" + "<hr width=20%>" + "<footer>" + json.quoteAuthor + "</footer>");
+          $("#tweet").attr("href","https://twitter.com/intent/tweet?text="+json.quoteText);
+      }
+  });
+};
+
+  ajaxFunction();
     $("#butn").on("click", function () {
-        $.ajax({
-            url: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
-            , cache: false
-            , dataType: "json"
-            , success: function (json) {
-                $(".quote").html("<p>" + json.quoteText + "</p>" + "<hr>" + "<footer>" + json.quoteAuthor + "</footer>");
-            }
-        });
-        $("#tweet").attr("href","https://twitter.com/intent/tweet?");
-    });
-    $("#tweet").on("click",function(){
-      console.log("button was clicked!");
+        ajaxFunction();
     });
 });
